b837f911071c366549a93bb4d36cd1ef396e3c36
Quar <70469187+QuarTheDev@users.noreply.github.com>
2023-07-13 07:12:26 -0400

    temp _ patch

--- cloudflare/projects/windows-upgrade-cli/downloads/script.js
+++ cloudflare/projects/windows-upgrade-cli/downloads/script.js
@@ -1,9 +1,9 @@
 const titleElement = document.getElementById('title');
 
 setInterval(() => {
-    if (titleElement.textContent.includes("█")) {
+    if (titleElement.textContent.includes("_")) {
         titleElement.textContent = "windows-upgrade-cli ";
     } else {
-        titleElement.textContent = "windows-upgrade-cli█";
+        titleElement.textContent = "windows-upgrade-cli_";
     }
 }, 500);
